f2bb5ebaa4dc31e12cf08d78da16bade44a8660c
Albert-Taylor <bestjunh@gmail.com>
2024-10-01 20:16:10 +0900

    wavIter

--- egs/reverb/s5/run_decode_all.sh
+++ egs/reverb/s5/run_decode_all.sh
@@ -1,33 +1,38 @@
 #!/usr/bin/env bash
 
-# #AlgName=WPEOverIVA_v0.1.4
-# # AlgName=CDRWPEOverIVA_v1.3.1
+AlgName=WPEOverIVA_v1.0.0
+# AlgName=CDRWPEOverIVA_v2.3.0
+# AlgName=CDRSVDWPEOverIVA_v0.0.0
 
-# for lambda_scale in 0.0001; do
-#     for lambda_unit in 0; do
-#         for lambda_null in 0; do
-#             for D in 2; do
-#                 for L in 8; do
-#                     for delta in 1e-06 ; do
-#                         for nIter in 1; do                            
-#                             ./run_decode.sh $AlgName 'lambda_scale_'$lambda_scale'_lambda_unit_'$lambda_unit'_lambda_null_'$lambda_null'_D_'$D'_L_'$L'_delta_'$delta'_nIter_'$nIter
-#                         done
-#                     done
-#                 done
-#             done
-#         done
-#     done
-# done
-
-
-AlgName=OfflineWPEOverIVA_v3.1.1
-for D in 2 3;do
-    for L in 8 12 20;do
-        for deltaWPE in 0.001 0.0001 1e-05;do
-            for deltaIVA in 0.001 1e-05 1e-10;do
-                ./run_decode.sh $AlgName 'D_'$D'_L_'$L'_deltaWPE_'$deltaWPE'_deltaIVA_'$deltaIVA
+for lambda_scale in 0.0001; do
+    for lambda_unit in 0; do
+        for lambda_null in 0; do
+            for D in 2; do
+                for L in 8; do
+                    for delta in 1e-10 ; do
+                        for wpe_delta in 1;do
+                            for nIter in 1; do
+                                for wavIter in 1 2;do
+                                    ./run_decode_v2.sh $AlgName $wavIter 'lambda_scale_'$lambda_scale'_lambda_unit_'$lambda_unit'_lambda_null_'$lambda_null'_D_'$D'_L_'$L'_delta_'$delta'_wpe_delta_'$wpe_delta'_nIter_'$nIter
+                                done
+                            done
+                        done
+                    done
+                done
             done
         done
     done
 done
+
+
+# AlgName=OfflineWPEOverIVA_v3.1.1
+# for D in 2;do
+#     for L in 8;do
+#         for deltaWPE in 0.001;do
+#             for deltaIVA in 0.001;do
+#                 ./run_decode.sh $AlgName 'D_'$D'_L_'$L'_deltaWPE_'$deltaWPE'_deltaIVA_'$deltaIVA
+#             done
+#         done
+#     done
+# done
 
--- egs/reverb/s5/show_wer.sh
+++ egs/reverb/s5/show_wer.sh
@@ -1,52 +1,55 @@
 #!/usr/bin/env bash
 
-# #optName1=WPEOverIVA_v0.1.4
-# # optName1=CDRWPEOverIVA_v2.0.0
+#optName1=WPEOverIVA_v0.1.4
+# optName1=CDRWPEOverIVA_v2.3.0
+optName1=CDRSVDWPEOverIVA_v0.0.0
 
-# for lambda_scale in 0.0001;do
-#     for lambda_unit in 0; do
-#         for lambda_null in 0; do
-#             for D in 2; do
-#                 for L in 8; do
-#                     for delta in 1e-06; do
-#                         for nIter in 1; do
-#                             optName2='lambda_scale_'$lambda_scale'_lambda_unit_'$lambda_unit'_lambda_null_'$lambda_null'_D_'$D'_L_'$L'_delta_'$delta'_nIter_'$nIter
-#                             test_set=$optName1'_'$optName2
-#                             test_sets="et_real_8ch_${test_set}"
-#                             # cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_*
-#                             #echo $test_set
-#                             far_wer=$(cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_far_room1 | awk '/%WER/ {print $2}')
-#                             near_wer=$(cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_near_room1 | awk '/%WER/ {print $2}')
-#                             #echo -e "$far_wer\t$near_wer"
-                            
-#                             echo -e "$far_wer"
-#                             echo -e "$near_wer"
-#                         done
-#                     done
-#                 done
-#             done
-#         done
-#     done
-done
-
-
-optName1=OfflineWPEOverIVA_v3.1.1
-for D in 2 3;do
-    for L in 8 12 20;do
-        for deltaWPE in 0.001 0.0001 1e-05;do
-            for deltaIVA in 0.001 1e-05 1e-10;do
-                optName2='D_'$D'_L_'$L'_deltaWPE_'$deltaWPE'_deltaIVA_'$deltaIVA
-                test_set=$optName1'_'$optName2
-                test_sets="et_real_8ch_${test_set}"
-                # cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_*
-                #echo $test_set
-                far_wer=$(cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_far_room1 | awk '/%WER/ {print $2}')
-                near_wer=$(cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_near_room1 | awk '/%WER/ {print $2}')
-                #echo -e "$far_wer\t$near_wer"
-
-                echo -e "$far_wer"
-                echo -e "$near_wer"
+for lambda_scale in 0.0001;do
+    for lambda_unit in 0; do
+        for lambda_null in 0; do
+            for D in 2; do
+                for L in 8; do
+                    for delta in 1e-10; do
+                        for wpe_delta in 1;do
+                            for nIter in 1; do
+                                optName2='lambda_scale_'$lambda_scale'_lambda_unit_'$lambda_unit'_lambda_null_'$lambda_null'_D_'$D'_L_'$L'_delta_'$delta'_wpe_delta_'$wpe_delta'_nIter_'$nIter
+                                test_set=$optName1'_'$optName2
+                                test_sets="et_real_8ch_${test_set}"
+                                # cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_*
+                                #echo $test_set
+                                far_wer=$(cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_far_room1 | awk '/%WER/ {print $2}')
+                                near_wer=$(cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_near_room1 | awk '/%WER/ {print $2}')
+                                #echo -e "$far_wer\t$near_wer"
+                                
+                                echo -e "$far_wer"
+                                echo -e "$near_wer"
+                            done
+                        done
+                    done
+                done
             done
         done
     done
 done
+
+
+# optName1=OfflineWPEOverIVA_v3.1.1
+# for D in 2;do
+#     for L in 8;do
+#         for deltaWPE in 0.001;do
+#             for deltaIVA in 0.001;do
+#                 optName2='D_'$D'_L_'$L'_deltaWPE_'$deltaWPE'_deltaIVA_'$deltaIVA
+#                 test_set=$optName1'_'$optName2
+#                 test_sets="et_real_8ch_${test_set}"
+#                 # cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_*
+#                 #echo $test_set
+#                 far_wer=$(cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_far_room1 | awk '/%WER/ {print $2}')
+#                 near_wer=$(cat exp/chain_tr_simu_8ch/tdnn1a_sp/decode_test_tg_5k_$test_sets/scoring_kaldi/best_wer_near_room1 | awk '/%WER/ {print $2}')
+#                 #echo -e "$far_wer\t$near_wer"
+
+#                 echo -e "$far_wer"
+#                 echo -e "$near_wer"
+#             done
+#         done
+#     done
+# done
