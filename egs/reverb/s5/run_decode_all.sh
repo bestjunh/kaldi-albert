3f31c11421b2bf9e30ef3fa2a5c106d76446a667
Albert-Taylor <bestjunh@gmail.com>
2024-10-03 10:11:20 +0900

    mod wpe frame

--- egs/reverb/s5/run_decode_all.sh
+++ egs/reverb/s5/run_decode_all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# AlgName=WPEOverIVA_v1.0.0
-AlgName=CDRWPEOverIVA_v2.4.0
-# AlgName=CDRSVDWPEOverIVA_v0.0.0
+# AlgName=WPEOverIVA_v3.1.2
+AlgName=CDRWPEOverIVA_v2.6.0
+# AlgName=CDRSVDWPEOverIVA_v0.3.1
 
 for lambda_scale in 0.0001; do
     for lambda_unit in 0; do
@@ -12,8 +12,10 @@ for lambda_scale in 0.0001; do
                     for delta in 1e-10 ; do
                         for wpe_delta in 1;do
                             for nIter in 1; do
-                                for wavIter in 2;do
-                                    ./run_decode_v2.sh $AlgName $wavIter 'lambda_scale_'$lambda_scale'_lambda_unit_'$lambda_unit'_lambda_null_'$lambda_null'_D_'$D'_L_'$L'_delta_'$delta'_wpe_delta_'$wpe_delta'_nIter_'$nIter
+                                for mod_wpe_frame in 1 5;do
+                                    for wavIter in 1 2;do                                        
+                                        ./run_decode_v2.sh $AlgName $wavIter 'lambda_scale_'$lambda_scale'_lambda_unit_'$lambda_unit'_lambda_null_'$lambda_null'_D_'$D'_L_'$L'_delta_'$delta'_wpe_delta_'$wpe_delta'_nIter_'$nIter'_mod_wpe_frame_'$mod_wpe_frame
+                                    done
                                 done
                             done
                         done
